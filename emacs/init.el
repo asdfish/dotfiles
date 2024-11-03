c661ccd7615aab45a44d53778f3a38a14db43efc
asdfish <lgray3420@gmail.com>
2024-11-02 20:44:19 -0400

    moved requires below package installs

--- emacs/init.el
+++ emacs/init.el
@@ -1,11 +1,10 @@
-(require 'powerline)
-
 (add-to-list 'load-path (format "%s/lisp" user-emacs-directory))
-
 
 (customize-save-variable 'custom-file (format "%s/lisp/custom.el" user-emacs-directory))
 
 (load "packages")
+
+(require 'powerline)
 
 (load "commands")
 (load "custom")
